4f512c2feb5a3e689b6c7e92a7672674db31fd27
deoktr <35725720+deoktr@users.noreply.github.com>
2024-12-01 10:35:21 +0100

    add: flake

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# flake.nix
+# Onix flake.nix
+
+Will require arround 23Gb in `/nix/store/`.
+
+## Usage
+
+Install and use:
+
+```bash
+git clone https://github.com/onix-sec/flake.nix && cd flake.nix
+nix develop
+```
+
+## TODO
+
+- Add a script to generate the list of tools with the link to their homepage
+
+## License
+
+Onix flake.nix is licensed under [MIT](./LICENSE).
---
+++ flake.nix
@@ -0,0 +1,394 @@
+{
+  description = "Nix Hack";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixsecpkgs = {
+      url = "github:deoktr/nixsecpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixsecpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              nixsecpkgs.overlays.default
+            ];
+          };
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            # from nixsecpkgs
+            awrbacs
+            buster
+            byp4xx
+            carbon14
+            cmsmap
+            crackhound
+            dfscoerce
+            finduncommonshares
+            git-dumper
+            gmsadumper
+            go-bhtool
+            go-evilarc
+            go-windapsearch
+            goldencopy
+            gosecretsdump
+            goshs
+            haiti
+            hakrevdns
+            hashonymize
+            jackit
+            keyt
+            pof
+            pywhisket
+            robotstester
+            shellerator
+            shuffledns
+            smartbrute
+            smtp-user-enum
+            sprayhound
+            sublist3r
+            toutatis
+            uberfile
+            waybackurls
+            webclientservicescanner
+            whatportis
+
+            # aclpwn
+            adidnsdump
+            aircrack-ng
+            amass
+            amber
+            androguard
+            android-tools # android-tools-adb
+            anew
+            python311Packages.angr
+            apksigner
+            apktool
+            arjun
+            arsenal
+            ascii
+            assetfinder
+            autoconf
+            # autorecon
+            avrdude
+            awscli
+            bettercap
+            binwalk
+            bloodhound
+            # bloodhound-import
+            # bloodhound-quickwin
+            # bloodhound.py
+            bolt
+            # bqm
+            brakeman
+            bruteforce-luks
+            bully
+            burpsuite
+            # byp4xx
+            # carbon14
+            certipy
+            certsync
+            cewl
+            # checksec-py
+            chisel
+            # cloudfail
+            # cloudmapper
+            python312Packages.cloudsplaining
+            # cloudsploit
+            # clusterd
+            # cmsmap
+            coercer
+            # constellation
+            # corscanner
+            cowpatty
+            # crackhound
+            crackmapexec
+            crunch
+            # cupp
+            # cyperoth
+            # darkarmour
+            dex2jar
+            # dfscoerce
+            dirb
+            # dirsearch
+            das # divideandscan
+            dns2tcp
+            dnschef
+            dnsenum
+            dnsx
+            donpapi
+            # droopescan
+            # drupwn
+            # eaphammer
+            enum4linux-ng
+            # enyx
+            evil-winrm
+            exif
+            exifprobe
+            exiftool
+            exiv2
+            # eyewitness
+            fcrackzip
+            feroxbuster
+            ffuf
+            fierce
+            # finalrecon
+            findomain
+            # finduncommonshares
+            firefox
+            foremost
+            freerdp3 # freerdp2-x11
+            frida-tools
+            # fuxploider
+            gau
+            # genusernames
+            gf
+            ghidra
+            # git-dumper
+            # githubemail
+            git # gittools
+            gobuster
+            # gopherus
+            # gosecretsdump
+            gowitness
+            # gpp-decrypt
+            gqrx
+            gron
+            # h2csmuggler
+            h8mail
+            hackrf
+            hakrawler
+            hashcat
+            # hashonymize
+            hcxdumptool
+            hcxtools
+            hexedit
+            holehe
+            hping # hping3
+            # httpmethods
+            httprobe
+            python312Packages.httpx
+            # hydra
+            # ida-free # FIXME: fails to build
+            # ignorant
+            imagemagick
+            python312Packages.impacket
+            # infoga
+            ipinfo
+            iptables
+            jadx
+            jd-gui
+            # jdwp
+            john
+            joomscan
+            # jwt-cli # jwt
+            jwt-hack
+            # kadimus
+            # KeePwn
+            kerbrute
+            kiterunner
+            # Kraken
+            # krbrelayx
+            kubectl
+            python312Packages.ldapdomaindump
+            # ldaprelayscan
+            openldap # ldapsearch
+            # ldapsearch-ad
+            ldeep
+            libmspack
+            libnfc
+            # libnfc # libnfc-crypto1-crack
+            libusb1
+            ligolo-ng
+            # linkedin2username
+            xnlinkfinder # linkfinder
+            # lnkup
+            python312Packages.lsassy # lsassy
+            ltrace
+            maigret
+            maltego
+            # manspider
+            libmysqlclient # mariadb-client
+            python312Packages.masky # masky
+            masscan
+            mdcat
+            metasploit
+            mfcuk
+            # mfdread
+            mfoc
+            minicom
+            mitm6
+            # moodlescan
+            # mousejack
+            # msprobe
+            naabu
+            python312Packages.name-that-hash # name-that-hash
+            nasm
+            nbtscan
+            neo4j
+            netdiscover
+            conntrack-tools # nfct
+            ngrok
+            nmap
+            # noPac
+            # nosqlmap
+            # ntlmv1-multi
+            nuclei
+            # oaburl
+            # objection
+            # objectwalker
+            # oneforall
+            onesixtyone
+            # osrframework
+            # pass
+            # PassTheCert
+            python311Packages.patator # patator
+            # pcredz
+            # pcsclite # pcsc
+            pdfcrack
+            # peepdf
+            # petitpotam
+            # phoneinfoga
+            photon
+            php
+            # filter
+            # chain
+            # generator
+            # phpggc
+            # pkinittools
+            # polenum
+            powershell
+            pre2k
+            # prips
+            # privexchange
+            prowler
+            proxmark3
+            proxychains
+            # pst-utils
+            # pth-tools
+            pwncat
+            # pwndb
+            pwndbg
+            # pwnedornot
+            pwninit
+            pwntools
+            # pygpoabuse
+            # pykek
+            # pylaps
+            python312Packages.pypykatz
+            # pyrit
+            # pywhisker
+            # pywsus
+            radare2
+            rdesktop
+            reaverwps
+            # recon-ng
+            # recondog
+            # redis-tools
+            remmina
+            responder
+            rlwrap
+            # roastinthemiddle
+            rockyou
+            routersploit
+            # rsactftool
+            rsync
+            rtl_433 # rtl-433
+            ruler
+            # rusthound
+            samdump2
+            scout
+            scrcpy
+            exploitdb # searchsploit
+            seclists
+            semgrep
+            # shadowcoerce
+            # simplyemail
+            sipvicious
+            sleuthkit
+            # sliver
+            # smali
+            samba # smbclient
+            smbmap
+            # smuggler
+            soapui # SoapUI
+            # spiderfoot
+            sqlmap
+            ssh-audit
+            sshuttle
+            sslscan
+            # ssrfmap
+            # steghide
+            # stegolsb
+            # stegosuite
+            strace
+            subfinder
+            swaks
+            # symfony-exploits
+            tailscale
+            # targetedKerberoast
+            tcpdump
+            testdisk
+            testssl
+            theharvester
+            # timing
+            # tls-map
+            # tls-scanner
+            # tomcatwardeployer
+            tor
+            traceroute
+            # trevorspray
+            # trid
+            trilium-desktop
+            tshark
+            updog
+            username-anarchy
+            util-linux
+            volatility3
+            # vulny-code-static-analysis
+            wabt
+            wafw00f
+            weevely
+            wfuzz
+            whatweb
+            whois
+            wifite2
+            wireshark
+            wpscan
+            wuzz
+            # XSpear
+            # xsrfprobe
+            xsser
+            # xsstrike
+            tightvnc # xtightvncviewer
+            yt-dlp
+            # ysoserial
+            # zerologon
+            # zipalign
+            zsteg
+
+            # added:
+            wordlists
+          ];
+
+          shellHook = ''
+            echo "Welcome to NixHack"
+          '';
+        };
+    };
+}
